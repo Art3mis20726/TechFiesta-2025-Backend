d0c141a2ba08d329fea998d884c19335201548d5
Art3mis20726 <gitpats@gmail.com>
2024-10-11 00:11:42 +0530

    "Added restricted URLs middleware to proxy server, blocking access to specific URLs"

--- Proxy-Server/proxy-server.js
+++ Proxy-Server/proxy-server.js
@@ -3,12 +3,17 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 const proxyTarget = 'https://techfiesta-backend.onrender.com';
-
+const restrictedUrls = ['/api/v1/users/pangs'];
+app.use((req, res, next) => {
+  if (restrictedUrls.includes(req.path)) {
+    return res.status(403).send('Access to this resource is forbidden.');
+  }
+  next();
+});
 app.use('/', createProxyMiddleware({
   target: proxyTarget,
   changeOrigin: true,
 }));
-
 // Start the proxy server
 app.listen(4000, () => {
   console.log('Proxy server is running on port 4000');
